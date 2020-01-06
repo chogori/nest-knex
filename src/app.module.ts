9d5fe168770059742ae29dac5e2cc8aa4eced58e
chogori <nikita.gromozdin@fib.sfedu.ru>
2020-01-07 02:02:28 +0300

    deleted useless comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { ConfigService } from './config/config.service';
 import { KnexModule } from '@nestjsplus/knex';
-// import { ConfigModule } from './config/config.module';
 import * as config from 'config';
 
 @Module({
   imports: [
-    // ConfigModule,
     KnexModule.register(config.get('knex')),
   ],
   controllers: [AppController],
