389aa949cef1f331579c8372812e36a2af06af4c
chogori <nikita.gromozdin@fib.sfedu.ru>
2020-01-14 22:55:22 +0300

    feat(webpack): added webpack compilation

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(config.get('app.port'));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
 }
 bootstrap();
